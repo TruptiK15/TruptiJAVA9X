8f1f86d14e25d114bdf95e068b7e67a9769c1d54
Trupti <truptikeni45@gmail.com>
2025-02-16 23:31:15 +0530

    testt

---
+++ src/October/ex_22102024_Loops/Loops_while.java
@@ -0,0 +1,19 @@
+package October.ex_22102024_Loops;
+
+public class Loops_while {
+    public static void main(String[] args) {
+        /* Loop - repeat a block of code
+        - While Loop - check condition first and execute code
+                initialization
+                while(Condition){
+                     //block of code
+                updateion
+            }
+        */
+        int i = 0;      //variable name can be anything i.e., i can be name also
+        while (i < 10) {
+            System.out.println(i);
+            i++;
+        }
+    }
+}
